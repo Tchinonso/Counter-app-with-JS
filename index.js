35ed498230a8553e1bcb32a2a889bab6962ef257
Tchinonso <lightersmercury@gmail.com>
2022-11-05 18:27:38 +0100

    counter app still in process

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-document.getElementById('count-el').innerText = 5
+//document.getElementById('count-el').innerText = 5
+
+//let count = 0
+//console.log(count)
+
+//  let myAge = 27
+//  console.log(myAge)
+
+// let myAge = 27
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// let bonusPoint = 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint - 75
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 45
+// console.log(bonusPoint)
+
+// const increment = () => {
+// 	console.log('button was clicked');
+// }
+
+// let hood = () => {
+// 	console.log(42)
+// }
+// hood()
+
+let lap1 = 31
+let lap2 = 33
+let lap3 = 36
+
+function myLap() {
+	console.log(lap1 + lap2 + lap3 )
+}
+myLap()
