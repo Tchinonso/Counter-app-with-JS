c21e08f14331d57f1691e9d7411506696345092e
Tchinonso <lightersmercury@gmail.com>
2022-11-07 14:20:30 +0100

    count button increment done

--- index.js
+++ index.js
@@ -53,9 +53,14 @@
 
 // console.log(lapsCompleted)
 
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
 let count = 0
 
 function increment() {
-	count = count + 1
-	console.log(count)
-}
+ 	count = count + 1
+ 	countEl.innerText = count
+ }
+
+
